1d67ce58dccb9eddaad49a0ef69740307aac6259
Hemavarna S <hemavarnas25@gmail.com>
2025-01-22 01:30:31 +0530

    The files of Day 8 AN has been added successfully
    This files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/SignUp";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css'
+import About from './components/FunctionalComponents/About';
+import Gallery from './components/FunctionalComponents/Gallery';
+import Home from './components/FunctionalComponents/Home';
+// import Contact from './components/FunctionalComponents/Contact';
+import Navbar from './components/FunctionalComponents/Navbar';
+import Signup from './components/FunctionalComponents/Signup';
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
+    <BrowserRouter>
+    <Navbar/>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+      <Route path="/"element={<Home />}></Route>
+      <Route path="/about"element={<About />}></Route>
+      <Route path="/gallery"element={<Gallery page="Gallery" image="SECE Logo" />}></Route>
+      {/* <Route path="/contact"element={<Contact />}></Route> */}
+      <Route path="/signup"element={<Signup />}></Route>
       </Routes>
-    </Router>
-  );
+    </BrowserRouter>
+  )
 }
 
 export default App;
